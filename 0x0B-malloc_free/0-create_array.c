59c1b06f6bc44a932761b04602a8d2076963109e
eziebo solomon <eziebosolomon2019@gmail.com>
2023-04-11 14:16:49 -0700

    loop

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	tab =(char *)malloc(sizeof(char) * size);
+	tab = (char *)malloc(sizeof(char) * size);
 	if (!tab)
 		return (NULL);
 
