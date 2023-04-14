fa28e8461a9802d9fe448eed7291257f15dd5021
eziebo solomon <eziebosolomon2019@gmail.com>
2023-04-14 15:35:08 -0700

    c program

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,41 +1,47 @@
-#include <stdlib.h> // Required for malloc function
-#include <string.h> // Required for strlen and strncpy functions
+#include "main.h"
+#include <stdio.h>
 
-char *string_nconcat(char *s1, char *s2, unsigned int n) {
-    // Treat NULL as an empty string
-    if (s1 == NULL) {
-        s1 = "";
-    }
-    if (s2 == NULL) {
-        s2 = "";
-    }
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: pointer to first string.
+ * @s2: pointer to 2nd string.
+ * @n: Number of bytes from n2 to concatenate.
+ *
+ * Return:Pointer to space in memory containing concatenated string.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i, j, s1_length, s2_length;
 
-    // Calculate length of s1 and s2
-    size_t s1_len = strlen(s1);
-    size_t s2_len = strlen(s2);
+	/*Check if the strings passed are null*/
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	/*Compute the length of the strings*/
 
-    // Use the entire s2 if n is greater or equal to its length
-    if (n >= s2_len) {
-        n = s2_len;
-    }
+	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
+		;
 
-    // Allocate memory for the concatenated string
-    char *concat = (char *)malloc((s1_len + n + 1) * sizeof(char));
+	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
+		;
+	/*Memory reservation-for case 1 & 2.*/
+	str = malloc(s1_length + n + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	/*Copy first string into str.*/
+	for (i = 0; s1[i] != '\0'; i++)
+		str[i] = s1[i];
+	/*copy second stringg into str.*/
+	for (j = 0; j < n; j++)
+	{
+		str[i] = s2[j];
+		i++;
+	}
 
-    // Check if memory allocation was successful
-    if (concat == NULL) {
-        return NULL;
-    }
-
-    // Copy s1 to the concatenated string
-    strncpy(concat, s1, s1_len);
-
-    // Copy the first n bytes of s2 to the concatenated string
-    strncpy(concat + s1_len, s2, n);
-
-    // Null-terminate the concatenated string
-    concat[s1_len + n] = '\0';
-
-    return concat;
+	str[i] = '\0';
+	return (str);
 }
-
